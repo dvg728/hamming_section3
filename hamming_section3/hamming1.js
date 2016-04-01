86118e2f14b3a506df75568e16dacea0cb59558f
dvg728 <gordondv@dukes.jmu.edu>
2016-04-01 10:17:14 -0400

    Added files via upload

---
+++ hamming_section3/hamming1.js
@@ -0,0 +1,19 @@
+var compute;
+
+// this function computes the Hamming
+// distance etween two strands of
+// nucleotides.
+compute = function(strand1, strand2) {
+	// declare our output variable
+	// default is identical strands,
+	//output the hamming distance
+	var distance = 0;
+
+	// calculate the actual Hamming distance
+
+	
+};
+
+module.exports = {
+	compute: compute
+}
