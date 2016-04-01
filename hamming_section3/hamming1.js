303cee4ec21a0670918de0031909b6c1d5aa8ac9
dvg728 <gordondv@dukes.jmu.edu>
2016-04-01 11:02:08 -0400

    Added files via upload

--- hamming_section3/hamming1.js
+++ hamming_section3/hamming1.js
@@ -4,13 +4,33 @@ var compute;
 // distance etween two strands of
 // nucleotides.
 compute = function(strand1, strand2) {
+	
+	//
+	if (s1.length !== s2.length) {
+		//then throw an error
+		throw new Error('DNA strands must be of equal lenght.');
+	}
+
 	// declare our output variable
 	// default is identical strands,
 	//output the hamming distance
 	var distance = 0;
 
-	// calculate the actual Hamming distance
+	// cd compute the hamming distance
+	// iterate over every pair of letters in 
+	// the two strands
+	for (var i = 0; i < s1.length; i += 1) {
+		// if the i'th character in strand 1 is NOT
+		// equal to the i'th character in strand 2
+		if (s1.charAt(i) !== s2.charAt(i)) {
+			// add 1 to the distance
+			dist += 1
+		}
+	}
 
+
+	// calculate the actual Hamming distance
+	return dist;
 	
 };
 
